5b5987f97f285ecb21f82ac1341b1bc5524440e8
rgodwin <me@rossgodwin.com>
2020-02-28 17:11:06 -0500

    Fixed logo referencing wrong image file

--- src/com/gwn/xcbl/web/AppServletContextUtils.java
+++ src/com/gwn/xcbl/web/AppServletContextUtils.java
@@ -5,7 +5,7 @@ import javax.servlet.ServletContext;
 public class AppServletContextUtils {
 
 	public static String getLogoRealPath(ServletContext ctx) {
-		String result = ctx.getRealPath("/assets/img/web_logo_rectangle_xs.png");
+		String result = ctx.getRealPath("/client/assets/img/client_logo_rectangle_xs.png");
 		return result;
 	}
 }
